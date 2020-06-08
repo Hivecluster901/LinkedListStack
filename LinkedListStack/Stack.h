ca99daf05297cefa06534b6169a463345f7426c2
Minjun1234 <bill981212@gmail.com>
2020-06-08 18:22:23 +0900

    Pop function seems to be not working

--- LinkedListStack/Stack.h
+++ LinkedListStack/Stack.h
@@ -2,9 +2,66 @@
 
 class Stack
 {
+private:
+	class Element
+	{
+	public:
+		Element(int val)
+			:
+			x(val)
+		{
+		}
+		int x;
+		Element* ptr_to_next = nullptr;
+	};
 public:
-	void Push( int val );
-	int Pop();
-	int Size() const;
-	bool Empty() const;
+	void Push(int val)
+	{
+		if (Empty())
+		{
+			Element* e = new Element(val);
+			firstPtr = e;
+		}
+		else
+		{
+			Element* e = new Element(val);
+			Element* ptr = firstPtr;
+			while (ptr->ptr_to_next != nullptr)
+			{
+				ptr = ptr->ptr_to_next;
+			}
+			ptr = e;
+		}
+	}
+	int Pop()
+	{
+		Element** ptr1 = nullptr;
+		Element* ptr2 = firstPtr;
+		while (ptr2->ptr_to_next != nullptr)
+		{
+			ptr2 = ptr2->ptr_to_next;
+		}
+		ptr1 = &ptr2;
+		int value = ptr2->x;
+		delete ptr2;
+		*ptr1 = nullptr;
+		return value;
+	}
+	int Size() const
+	{
+		Element* ptr = firstPtr;
+		int size = 0;
+		while (ptr->ptr_to_next != nullptr)
+		{
+			ptr = ptr->ptr_to_next;
+			size++;
+		}
+		return size;
+	}
+	bool Empty() const
+	{
+		return firstPtr == nullptr;
+	}
+private:
+	Element* firstPtr = nullptr;
 };
